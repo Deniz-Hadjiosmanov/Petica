3b46b29718779d0f22c5ce0a12031823a70e6852
jair <i451668@fhict.nl>
2022-12-12 14:15:45 +0100

    Modified contact page messagescript js to be able to clear input on message sent

--- website/messageScript.js
+++ website/messageScript.js
@@ -2,8 +2,21 @@
 
 var form = document.getElementById('contactForm');
 
+/*adds an event listener on the form section*/
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    alert("Your message has been sent");
-   
+    /*variables created for each id, so these can be used in the function*/
+    const usernameInput = document.getElementById('username');
+    const emailInput = document.getElementById('email');
+    const textAreaInput = document.getElementById('textarea');
+    /*displays that a message has been sent*/
+    if(alert("Your message has been sent")){
+        console.log(usernameInput.value);
+        console.log(emailInput.value);
+        console.log(textAreaInput.value);
+    };
+    /*clears the form input fields after showing the alert message*/
+    usernameInput.value = '';
+    emailInput.value = '';
+    textAreaInput.value = '';
 })
