fd97606daee0e8e86285b56e576c533bebc2609d
jair <i451668@fhict.nl>
2023-01-06 15:47:58 -0400

    Changed bird description for the steps

--- website/bird_script.js
+++ website/bird_script.js
@@ -109,61 +109,26 @@ function showProgress() {
 function showScores() {
     //console.log(quiz.score)
     if (quiz.score >= 4) {
-        // Cat that sheds
+        // Big birds
         let quizEndHTML =
-            `
-            <h2 class='score'> YOUR IDEAL BIRD:</h2>
-            <div class="wrapper">
-            <div class="main_fish">
-           <center>
-            <img class="fish_img" src="images/bird/africanGrey1">
-            
-            <h3 class='score'> AFRICAN GREY</h3>
-            <p class='score'> African greys are known as having the outstanding ability to earn a human word and imitate speech, they earned the title "The Einstein of the Bird World".
-             This might be the perfect pet for you, since they can bond strongly with you and learn positive reinforcement training from you , as his new owner.</p>
-            </center>
-            </div>
-           
-            <div class="other-consider">
-            <h3 class='score'> ALSO CONSIDER</h3>
-            <img class="fish_img2" src="images/bird/parrot1.png">
-            <h4 class='score'>Parrot</h4>
-            <img class="fish_img2" src="images/bird/cockatiel1.png">
-            <h4 class='score'>Cockatiel</h4>
-            </div>
-    
-           
-            </div>
-           
-            <br>
-            <br>
-            <div class="quiz-repeat">
-            <a href="shops.html">Find my pet</a>
-            </div>
-        `;
-        let quizElement = document.getElementById("quiz");
-        quizElement.innerHTML = quizEndHTML;
-    }
-    // Cat that doesn't shed
-    else {
-        let quizEndHTML =
-            `
+        `
         <h2 id='score'> YOUR IDEAL BIRD:</h2>
         <div class="wrapper">
         <div class="main_fish">
         <center>
-        <img class="fish_img" src="images/bird/parkeet1.png">
-        <h3 id='score'>Parakeet</h3>
-        <p id='score'>Parakeets are one of the top birds worldwide as a pet, they are tiny parrot versions with long tail feathers.
-        This may be the perfect pet for you,since they can be trained on how to talk and also perform tricks. This pet bird is also pretty low-maintenance. </p>
+        <img class="fish_img" src="images/bird/africangrey1.png">
+        <h3 id='score'>African Grey</h3>
+        <p id='score'>
+        African greys are known as having the outstanding ability to earn a human word and imitate speech, they earned the title "The Einstein of the Bird World".
+        This might be the perfect pet for you, since they can bond strongly with you and learn positive reinforcement training from you , as his new owner.</p>
         </center>
         </div>
         <div class="other-consider">
         <h3 id='score'> ALSO CONSIDER</h3>
-        <img class="fish_img2" src="images/cat/cornish_rex_cat.png">
-        <h4 id='score'> Cornish Rex Cat</h4>
-        <img class="fish_img2" src="images/cat/sphynx_cat.png">
-        <h4 id='score'> Sphynx Cat</h4>
+        <img class="fish_img2" src="images/bird/conures1.png">
+        <h4 id='score'> Canaries</h4>
+        <img class="fish_img2" src="images/bird/macaws1.png">
+        <h4 id='score'> LoveBirds</h4>
         </div>
 
        
@@ -174,6 +139,42 @@ function showScores() {
         </div>
         
         `;
+        let quizElement = document.getElementById("quiz");
+        quizElement.innerHTML = quizEndHTML;
+    }
+    // small birds
+    else {
+        let quizEndHTML =
+        `
+        <h2 class='score'> YOUR IDEAL BIRD:</h2>
+        <div class="wrapper">
+        <div class="main_fish">
+       <center>
+        <img class="fish_img" src="images/bird/parkeet1.png">
+        
+        <h3 class='score'> Parakeet</h3>
+         Parakeets are one of the top birds worldwide as a pet, they are tiny parrot versions with long tail feathers.
+        This may be the perfect pet for you,since they can be trained on how to talk and also perform tricks. This pet bird is also pretty low-maintenance. </p>
+        </center>
+        </div>
+       
+        <div class="other-consider">
+        <h3 class='score'> ALSO CONSIDER</h3>
+        <img class="fish_img2" src="images/bird/parrot1.png">
+        <h4 class='score'>Parrot</h4>
+        <img class="fish_img2" src="images/bird/cockatiel1.png">
+        <h4 class='score'>Cockatiel</h4>
+        </div>
+
+       
+        </div>
+       
+        <br>
+        <br>
+        <div class="quiz-repeat">
+        <a href="shops.html">Find my pet</a>
+        </div>
+    `;
         let quizElement = document.getElementById("quiz");
         quizElement.innerHTML = quizEndHTML;
 
