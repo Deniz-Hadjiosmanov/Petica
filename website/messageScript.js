15120b5f69dbeca40fac8548570d43a6129f337b
Deniz Hadjiosmanov <d.hadjiosmanov@student.fontys.nl>
2023-01-08 21:07:11 +0100

    Contact page code readability improvements

--- website/messageScript.js
+++ website/messageScript.js
@@ -10,7 +10,7 @@ form.addEventListener('submit', (e) => {
     const emailInput = document.getElementById('email');
     const textAreaInput = document.getElementById('textarea');
     /*displays that a message has been sent*/
-    if(alert("Your message has been sent")){
+    if (alert("Your message has been sent")) {
         console.log(usernameInput.value);
         console.log(emailInput.value);
         console.log(textAreaInput.value);
